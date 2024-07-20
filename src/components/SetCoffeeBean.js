2c9e2fc16c510de51c90f3cafa5e25334da5da1f
3alifaisal <alifaisalawada000@gmail.com>
2024-07-20 16:03:33 +0200

    done Context with jest
    learned more about React custom hooks and experience using jest

--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,21 +1,21 @@
+import { useCoffee } from "../context/CoffeeContext";
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { setCoffeeBeanId } = useCoffee();
+  const onChangeOfCoffeeBean = (event) => {
+    setCoffeeBeanId(event.target.value);
+  };
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
-      <select
-        name="coffee-bean"
-      >
-        {coffeeBeans.map(bean => (
-          <option
-            key={bean.id}
-            value={bean.id}
-          >
+      <select name="coffee-bean" onChange={onChangeOfCoffeeBean}>
+        {coffeeBeans.map((bean) => (
+          <option key={bean.id} value={bean.id}>
             {bean.name}
           </option>
         ))}
       </select>
     </div>
   );
-}
+};
 
 export default SetCoffeeBean;
